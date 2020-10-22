63bd38336f79937e9600010e68c10a99246791bb
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2020-10-22 14:09:06 -0400

    hpcc4j-343 stage

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
 FROM hpccsystems/platform:latest
 
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt clean -y && \
+    apt autoclean -y && \
+    apt install -y -f && \
+    apt autoremove -y && \
+    apt-get update -y
+
+RUN apt-get install -y \
+    maven \
+    git
+
 COPY startup.sh /startup.sh
 
 ENTRYPOINT ["/startup.sh"]
--- startup.sh
+++ startup.sh
@@ -2,3 +2,15 @@
 
 #startup hpccsystems platform
 /etc/init.d/hpcc-init start
+
+git clone $1 hpcc4j
+cd hpcc4j
+mvn --batch-mode -Pbenchmark -Dmaven.test.skip=false \
+    -Dmaven.gpg.skip=true clean install -e \
+    -Dmaven.test.failure.ignore=false \
+    -Dhpccconn=http://localhost:8010 \
+    -Dwssqlport=8510 -Dmaven.javadoc.skip=true \
+    --file pom.xml
+
+results="$(cat dfsclient/results.json)"
+echo "::set-output name=benchmark-results::$results"
