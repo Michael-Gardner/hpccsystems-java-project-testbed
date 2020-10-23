713f80db155b66632eee48fe021eece2f867804a
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2020-10-23 14:31:37 -0400

    44

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 
 #startup hpccsystems platform
 /etc/init.d/hpcc-init start
-
+ls -la
 # generate ecl data needed for benchmark tests
 /generatedata.sh
-
+ls -la
 result_file="${INPUT_RESULTFILE}"
 printenv
 git clone --branch ${GITHUB_REF##[a-zA-Z0-9]*\/} ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY} clone-repo
