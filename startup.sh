cff4e1325c734ac1873a0879521b13f9941ca15b
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2020-10-23 14:22:37 -0400

    55

--- startup.sh
+++ startup.sh
@@ -7,9 +7,11 @@
 /generatedata.sh
 
 result_file="${INPUT_RESULTFILE}"
-#printenv
+printenv
 git clone --branch ${GITHUB_REF##[a-zA-Z0-9]*\/} ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY} clone-repo
 cd clone-repo
+whoami
+ls -la
 mvn --batch-mode -Pbenchmark -Dmaven.test.skip=false \
     -Dmaven.gpg.skip=true clean install -e \
     -Dmaven.test.failure.ignore=false \
